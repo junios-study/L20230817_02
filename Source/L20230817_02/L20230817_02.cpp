c8429899d4893b3af48f46f03dba311a08465de8
junios <junios@naver.com>
2023-08-17 17:22:49 +0900

    C++ 프로젝트 설정

---
+++ Source/L20230817_02/L20230817_02.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "L20230817_02.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, L20230817_02, "L20230817_02" );
---
+++ Source/L20230817_02/MyGameModeBase.cpp
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyGameModeBase.h"
+
+AMyGameModeBase::AMyGameModeBase()
+{
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Red,
+			TEXT("안녕하세요."));
+	}
+
+	UE_LOG(LogTemp, Warning, TEXT("Hello World"));
+
+	//NewObject<>()
+	//GetWorld()->SpawnActor<>
+		
+}
+
+void AMyGameModeBase::A()
+{
+}
